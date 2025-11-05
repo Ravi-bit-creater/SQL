4cbd611e24a8e383380bc7a8aa628b8e40256cc6
Ravi-bit-creater <ravimanikandanm@gmail.com>
2025-11-05 23:32:12 +0530

    All files are added

---
+++ practice_sql.sql
@@ -0,0 +1,46 @@
+
+CREATE TABLE CustomerTransactions (
+    id INT PRIMARY KEY,
+    login_device VARCHAR(50),
+    customer_name VARCHAR(100),
+    ip_address VARCHAR(20),
+    product VARCHAR(100),
+    amount DECIMAL(10, 2),
+    is_placed BOOLEAN,
+    is_viewed BOOLEAN,
+    transaction_status VARCHAR(20)
+);
+
+INSERT INTO CustomerTransactions VALUES
+(1, 'Mobile', 'Ravi', '192.168.1.1', 'Laptop', 50000.00, TRUE, FALSE, 'Completed'),
+(2, 'Desktop', 'Priya', '192.168.1.2', 'Smartphone', 20000.00, TRUE, TRUE, 'Completed'),
+(3, 'Tablet', 'Arjun', '192.168.1.3', 'Headphones', 1500.00, FALSE, TRUE, 'Failed'),
+(4, 'Mobile', 'Meena', '192.168.1.4', 'Shoes', 2500.00, TRUE, FALSE, 'Completed'),
+(5, 'Desktop', 'Karthik', '192.168.1.5', 'Watch', 5000.00, TRUE, TRUE, 'Completed'),
+(6, 'Mobile', 'Sowmya', '192.168.1.6', 'Tablet', 15000.00, TRUE, TRUE, 'Completed'),
+(7, 'Tablet', 'Ramesh', '192.168.1.7', 'Smartphone', 25000.00, FALSE, TRUE, 'Failed'),
+(8, 'Desktop', 'Divya', '192.168.1.8', 'Laptop', 60000.00, TRUE, FALSE, 'Completed'),
+(9, 'Mobile', 'Arun', '192.168.1.9', 'Smartwatch', 12000.00, TRUE, TRUE, 'Completed'),
+(10, 'Tablet', 'Deepa', '192.168.1.10', 'Laptop', 55000.00, FALSE, FALSE, 'Pending');
+
+SELECT * FROM customertransactions;
+
+SELECT COUNT(*) FROM customertransactions WHERE transaction_status NOT IN ('Failed','Pending');
+
+SELECT avg(amount) AS avg_transaction FROM CustomerTransactions;
+
+SELECT MIN(amount) AS min_transaction FROM customertransactions;
+
+SELECT max(amount) AS max_transaction FROM customertransactions;
+
+SELECT sum(amount) AS sum_transaction FROM customertransactions;
+
+SELECT login_device, sum(amount) AS sum_transaction
+FROM customertransactions
+GROUP BY login_device;
+
+SELECT login_device, sum(amount) AS sum_transaction
+FROM customertransactions
+GROUP BY login_device
+HAVING sum(amount) > 80000;
+
